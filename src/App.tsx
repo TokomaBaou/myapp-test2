890e1ac89d4aed2790ded2f7fa407eb433ef63d0
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-27 15:00:26 +0900

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Link } from "react-router-dom";
 import "./styles.css";
+import { Router } from "./components/router/Router";
 
 export default function App() {
   return (
     <ChakraProvider>
-      <h1>Hello,World!!</h1>
-      <h2>Enjoy</h2>
+      <BrowserRouter>
+        <Link to="/home">Home</Link>
+        <br />
+        <Link to="/setting">Setting</Link>
+        <br />
+        <br />
+        <h1>Hello,World!!</h1>
+        <h2>Enjoy</h2>
+
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -1,11 +1,21 @@
 import { memo, VFC } from "react";
 import { Route, Switch } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Setting } from "../pages/Setting";
+import { Top } from "../pages/Top";
 
 export const Router: VFC = memo(() => {
   return (
     <Switch>
-      <Route exact path="/"></Route>
-      <Route path="/home"></Route>
+      <Route exact path="/">
+        <Top />
+      </Route>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/setting">
+        <Setting />
+      </Route>
     </Switch>
   );
 });
