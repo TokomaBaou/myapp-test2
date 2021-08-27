c7071a71c73cc74d55049ee63b434bfaf6b2fd21
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-27 14:02:51 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <ChakraProvider>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </ChakraProvider>
+  );
+}
