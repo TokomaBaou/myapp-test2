9ef0b0ff85b46cec5254dd41f9a44b4f0b75e2ef
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-30 17:32:19 +0900

    boolean 記述変更

--- src/components/router/HomeRoutes.tsx
+++ src/components/router/HomeRoutes.tsx
@@ -1,5 +1,7 @@
 import { Home } from "../pages/Home";
+import { Page404 } from "../pages/Page404";
 import { Setting } from "../pages/Setting";
+import { UserManagement } from "../pages/userManagement";
 
 export const homeRoutes = [
   {
@@ -11,10 +13,15 @@ export const homeRoutes = [
     path: "/setting",
     exact: false,
     children: <Setting />
+  },
+  {
+    path: "/user_management",
+    exact: false,
+    children: <UserManagement />
+  },
+  {
+    path: "*",
+    exact: false,
+    children: <Page404 />
   }
-  // {
-  //   path:"",
-  //   exact:,
-  //   children:,
-  // }
 ];
--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -4,6 +4,8 @@ import { Home } from "../pages/Home";
 import { Setting } from "../pages/Setting";
 import { Top } from "../pages/Top";
 import { HeaderLayout } from "../templates/HeaderLayout";
+import { homeRoutes } from "./HomeRoutes";
+import { Page404 } from "../pages/Page404";
 
 export const Router: VFC = memo(() => {
   return (
@@ -11,17 +13,29 @@ export const Router: VFC = memo(() => {
       <Route exact path="/">
         <Top />
       </Route>
-      <Route path="/home">
-        <Switch>
-          {/* <Home /> */}
-          <Route>
-            <HeaderLayout></HeaderLayout>
-          </Route>
-        </Switch>
-      </Route>
-      <Route path="/setting">
-        <Setting />
+      {/* urlの中に/homeの値が入る */}
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {homeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="*">
+        <Page404 />
       </Route>
     </Switch>
+    // <Route path="/setting">
+    //   <Setting />
+    // </Route>
   );
 });
